cf49f049c0f72ae92d75aa875e672f5616ed2622
BorisFaj <boris88ab@gmail.com>
2025-03-16 19:28:06 +0100

    minnor fixes

--- kafka/server.py
+++ kafka/server.py
@@ -10,14 +10,13 @@ from dotenv import load_dotenv, find_dotenv
 import os
 import atexit
 
-# Cargar variables de entorno
 load_dotenv(find_dotenv(".env"))
 
-# Configuración de logs
+# logs
 sys.stdout.reconfigure(line_buffering=True)
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
-# Configuración de Kafka
+# Kafka
 producer = KafkaProducer(
     bootstrap_servers=os.environ.get("BOOTSTRAP_SERVER"),
     security_protocol="SASL_SSL",
@@ -27,14 +26,19 @@ producer = KafkaProducer(
 )
 hostname = str.encode(socket.gethostname())
 
-# Configuración AWS
+# AWS
 AWS_REGION = "eu-north-1"
 cloudwatch = boto3.client("cloudwatch", region_name=AWS_REGION)
 lambda_client = boto3.client("lambda", region_name=AWS_REGION)
 
-# Inicializar Flask
+# Flask
 app = Flask(__name__)
 
+def on_success(metadata):
+    app.logger.info(f"✅ Enviado a '{metadata.topic}' en offset {metadata.offset}")
+
+def on_error(e):
+  app.logger.info(f"❌ Error enviando mensaje a Kafka: {e}")
 
 def verificar_webhook():
     mode = request.args.get("hub.mode")
@@ -52,50 +56,39 @@ def verificar_webhook():
 
 def get_topic(data):
     """Determina el topic correcto del mensaje."""
-    if "entry" not in data or not isinstance(data["entry"], list) or len(data["entry"]) == 0:
-        raise ValueError("Formato de mensaje incorrecto: Falta 'entry'")
 
     changes = data["entry"][0].get("changes", [])
-    if not changes or not isinstance(changes, list):
-        raise ValueError("Formato de mensaje incorrecto: Falta 'changes' en 'entry'")
-
     value = changes[0].get("value", {})
-    if "messages" not in value or not isinstance(value["messages"], list) or len(value["messages"]) == 0:
-        raise ValueError("Formato de mensaje incorrecto: Falta 'messages' en 'value'")
 
-    # Extraer el mensaje real
+    if "statuses" in value:
+        return "whatsapp-text"
+
     message = value["messages"][0]
     message_type = message.get("type")
 
-    # Determinar el topic correcto
     if message_type == "text":
         return "whatsapp-text"
     elif message_type == "audio":
         return "whatsapp-audio"
     else:
-        raise ValueError(f"Tipo de mensaje no soportado: {message_type}")
-
+        return "whatsapp-text"
 
 def send_to_kafka(data, topic):
     """Encola el mensaje en Kafka."""
-    try:
+
+    for i in range(1):
         future = producer.send(
             topic,
             key=hostname,
-            value=str.encode(json.dumps(data))
+            value=str.encode(data)
         )
-        future.add_callback(lambda metadata: print(f"✅ Enviado a '{metadata.topic}' en offset {metadata.offset}"))
-        future.add_errback(lambda e: print(f"❌ Error enviando mensaje a Kafka: {e}"))
-
-        producer.flush()
-    except Exception as e:
-        print(f"❌ Error al encolar mensaje en Kafka: {e}")
-        raise
-
+        future.add_callback(on_success)
+        future.add_errback(on_error)
+    producer.flush()
 
 def enviar_metricas():
     """Envía a CloudWatch la cantidad de mensajes procesados en Kafka."""
-    print("📊 Enviando métrica a CloudWatch...")
+    app.logger.info("📊 Enviando métrica a CloudWatch...")
 
     try:
         response = cloudwatch.put_metric_data(
@@ -111,15 +104,15 @@ def enviar_metricas():
             ]
         )
 
-        print(f"✅ Métrica enviada con éxito. Respuesta: {response}")
+        app.logger.info(f"✅ Métrica enviada con éxito. Respuesta: {response}")
 
     except Exception as e:
-        print(f"❌ Error al enviar métrica a CloudWatch: {e}")
+        app.logger.info(f"❌ Error al enviar métrica a CloudWatch: {e}")
 
 
 def invoke_lambda():
     """Llama a la Lambda para encender la instancia si está apagada."""
-    print("🔹 Intentando invocar la Lambda desde Flask...")
+    app.logger.info("🔹 Intentando invocar la Lambda desde Flask...")
     try:
         payload = {"source": "flask"}
         response = lambda_client.invoke(
@@ -128,7 +121,7 @@ def invoke_lambda():
             Payload=json.dumps(payload)
         )
         result = response["Payload"].read()
-        print(f"✅ Lambda ejecutada con éxito. Respuesta: {result}")
+        app.logger.info(f"✅ Lambda ejecutada con éxito. Respuesta: {result}")
     except Exception as e:
         raise Exception(f"❌ Error al invocar la Lambda: {e}")
 
@@ -143,37 +136,31 @@ def webhook():
             if request.content_type != "application/json":
                 return jsonify({"status": "error", "message": "Unsupported Media Type"}), 415
 
-            data = request.get_json(silent=True)  # Evitar que Flask lance una excepción si no es JSON
+            data = request.get_json(silent=True)
             if not data:
                 return jsonify({"status": "error", "message": "No JSON received"}), 400
 
-            print("📩 Mensaje recibido:", json.dumps(data, indent=2))
+            app.logger.info("📩 Mensaje recibido:", json.dumps(data, indent=2))
 
-            # Obtener el topic correcto
             topic = get_topic(data)
-
-            # Enviar mensaje a Kafka
             send_to_kafka(data, topic)
-
-            # Enviar métricas y llamar a Lambda
             enviar_metricas()
             # invoke_lambda()
 
             return jsonify({"status": "success", "topic": topic}), 200
 
         except Exception as e:
-            print("❌ Error:", str(e))
+            app.logger.info("❌ Error:", str(e))
             return jsonify({"status": "error", "message": str(e)}), 500
 
-    return jsonify({"status": "error", "message": "Invalid request"}), 400  # En caso de que no entre en GET o POST
+    return jsonify({"status": "error", "message": "Invalid request"}), 400
 
 
 def cerrar_kafka_producer():
     """Cierra el Kafka Producer al finalizar el programa."""
-    print("🔴 Cerrando Kafka Producer...")
+    app.logger.info("🔴 Cerrando Kafka Producer...")
     producer.close()
 
-# Asegurar que el producer se cierre correctamente al apagar Flask
 atexit.register(cerrar_kafka_producer)
 
 if __name__ == '__main__':
