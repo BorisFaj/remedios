a7453ebb1976f84628cc10b7b0805bef8a82ff2c
boris <boris.abct@gmail.com>
2025-12-09 18:35:03 +0100

    fix path

--- remedios/apis/audio/whispercpp_launcher.py
+++ remedios/apis/audio/whispercpp_launcher.py
@@ -94,7 +94,7 @@ def main() -> None:
     app_cmd = [
         sys.executable,
         "-m",
-        "remedios.apis.audio_service",
+        "remedios.apis.audio.whispercpp_service",
         "--host",
         app_host,
         "--port",
